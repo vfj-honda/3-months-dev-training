d22ee456b57b98ee6b47e91f130310d367a7b919
Honda Masahiro <mhonda@vitalify.jp>
2021-05-27 14:28:15 +0900

    ディレクトリ構成の変更。UserとAdminの分離

--- webapp/app/Http/Controllers/User/HomeController.php
+++ webapp/app/Http/Controllers/User/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -8,22 +8,13 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
-        return view('auth/home');
+        return view('home');
     }
+
 }

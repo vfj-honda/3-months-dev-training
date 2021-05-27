1ec00dbcd458190cbba0ea599d1f40b82c1b7930
Honda Masahiro <mhonda@vitalify.jp>
2021-05-27 15:08:45 +0900

    admin/ 以下のルート作成

--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -17,5 +17,13 @@ Route::get('/', 'User\HomeController@index')->name('user.home');
 Route::prefix('admin')->group(function (){
     Route::get('/', 'Admin\HomeController@index')->name('admin.home');
     Auth::routes();
+
+    Route::get('/employee/create', 'Admin\EmployeeController@create')->name('admin.employee.create');
+    Route::get('/employee/list', 'Admin\EmployeeController@show')->name('admin.employee.list');
+
+    Route::get('/notification', 'Admin\NotificationController@edit')->name('admin.notification.edit');
+    Route::get('/notification/logs', 'Admin\NotificationController@logs')->name('admin.notification.logs');
+
+    Route::get('/logs', 'Admin\LogController@show')->name('admin.log.list');
 });
 

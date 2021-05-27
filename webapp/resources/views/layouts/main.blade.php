26c54da373abacb2aae4641f17af7cb0c1d7bf99
Honda Masahiro <mhonda@vitalify.jp>
2021-05-27 12:59:37 +0900

    画面作成

---
+++ webapp/resources/views/layouts/main.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ブログ管理サイト -vitalify-</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>

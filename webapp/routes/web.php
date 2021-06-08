37906c34deca68761b8c87df9117f2c29de95b2b
Honda Masahiro <mhonda@vitalify.jp>
2021-06-08 18:07:30 +0900

    ルート修正

--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -13,41 +13,43 @@
 
 # ルートにアクセスしたら/{year}/{month}にredirect 
 
-// Route::group(['prefix' => 'admin'], function (){
     
-    Auth::routes();
-    
-    Route::group(['prefix' => '/admin', 'middleware' => 'auth'], function(){
+Auth::routes();
 
-        Route::get('/', 'Admin\HomeController@index')->name('admin.home');
+Route::group(['prefix' => '/admin', 'middleware' => 'auth'], function(){
 
-        Route::group(['prefix' => '/employee'], function(){
+    Route::get('/', 'Admin\HomeController@index')->name('admin.home');
 
-            Route::get('/create', 'Admin\EmployeeController@create')->name('admin.employee.create');
-            Route::post('/create', 'Admin\EmployeeController@store')->name('admin.employee.store');
-    
-            Route::get('/edit/{id}', 'Admin\EmployeeController@edit')->name('admin.employee.edit');
-            Route::put('/update/{id}', 'Admin\EmployeeController@update')->name('admin.employee.update');
-    
-            Route::get('/list', 'Admin\EmployeeController@show')->name('admin.employee.list');
-            Route::delete('/destroy', 'Admin\EmployeeController@destroy')->name('admin.employee.destroy');
-    
-            Route::get('/orders_list', 'Admin\OrderController@show')->name('admin.employee.order_list');
-            Route::put('/orders_list/switch', 'Admin\OrderController@switch')->name('admin.employee.order_list.switch');
-            
-        });
+    Route::group(['prefix' => '/employee'], function(){
+
+        Route::get('/create', 'Admin\EmployeeController@create')->name('admin.employee.create');
+        Route::post('/create', 'Admin\EmployeeController@store')->name('admin.employee.store');
+
+        Route::get('/edit/{id}', 'Admin\EmployeeController@edit')->name('admin.employee.edit');
+        Route::put('/update/{id}', 'Admin\EmployeeController@update')->name('admin.employee.update');
+
+        Route::get('/list', 'Admin\EmployeeController@show')->name('admin.employee.list');
+        Route::delete('/destroy', 'Admin\EmployeeController@destroy')->name('admin.employee.destroy');
+
+        Route::get('/authority', 'Admin\EmployeeController@authority')->name('admin.employee.authority');
+        Route::put('/authority/elevate', 'Admin\EmployeeController@elevate')->name('admin.employee.authority.elevate');
+        Route::put('/authority/diselevate', 'Admin\EmployeeController@diselevate')->name('admin.employee.authority.diselevate');
+
+        Route::get('/orders_list', 'Admin\OrderController@show')->name('admin.employee.order_list');
+        Route::put('/orders_list/switch', 'Admin\OrderController@switch')->name('admin.employee.order_list.switch');
         
-        Route::get('/notification/edit', 'Admin\NotificationController@edit')->name('admin.notification.edit');
-        Route::put('/notification/update', 'Admin\NotificationController@update')->name('admin.notification.update');
-        Route::get('/notification/logs', 'Admin\NotificationController@logs')->name('admin.notification.logs');
-
-        Route::post('/skip/create', 'Admin\SkipController@create')->name('admin.skip.create');
-        Route::delete('/skip/destroy', 'Admin\SkipController@destroy')->name('admin.skip.destroy');
-        
-        Route::get('/logs', 'Admin\LogController@show')->name('admin.log.list');
     });
     
-// });
+    Route::get('/notification/edit', 'Admin\NotificationController@edit')->name('admin.notification.edit');
+    Route::put('/notification/update', 'Admin\NotificationController@update')->name('admin.notification.update');
+    Route::get('/notification/logs', 'Admin\NotificationController@logs')->name('admin.notification.logs');
+
+    Route::post('/skip/create', 'Admin\SkipController@create')->name('admin.skip.create');
+    Route::delete('/skip/destroy', 'Admin\SkipController@destroy')->name('admin.skip.destroy');
+    
+    Route::get('/logs', 'Admin\LogController@show')->name('admin.log.list');
+});
+
 
 Route::get('/', 'User\HomeController@root')->name('user.root');
 Route::get('/dashboard/{year}/{month}', 'User\HomeController@index')->name('user.home');

8258e3a2c84875fc4eb08d2dd4ea8305bcf531c4
Honda Masahiro <mhonda@vitalify.jp>
2021-06-08 18:08:31 +0900

    バッチ処理のためのログを追加

--- webapp/config/logging.php
+++ webapp/config/logging.php
@@ -54,6 +54,12 @@ return [
             'days' => 14,
         ],
 
+        'batch' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/batch.log'),
+            'level' => 'info',
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
